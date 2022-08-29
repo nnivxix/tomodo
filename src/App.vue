90c1910ab526ecf9e022114e65d4c28d8f604fa4
nnivxix <adhe.hanasa@gmail.com>
2022-08-30 05:21:08 +0700

    conditional value todos

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,29 @@
 <script setup>
-import TitleApp from './components/TitleApp.vue';
-import TheInformation from './components/TheInformation.vue';
-import ItemTodo from './components/ItemTodo.vue';
-import { VueFinalModal,  } from 'vue-final-modal'
-import { ref,  onMounted } from 'vue';
-import MyForm from './components/MyForm.vue';
-import {
-  todos, deleteTodo, getTodo, doneTodoToggle, todoHasDone, editTodo, isEditing, todoItem
-  } from './composable/todo'
+  import TitleApp from './components/TitleApp.vue';
+  import TheInformation from './components/TheInformation.vue';
+  import ItemTodo from './components/ItemTodo.vue';
+  import { VueFinalModal,  } from 'vue-final-modal'
+  import { ref,  onMounted } from 'vue';
+  import MyForm from './components/MyForm.vue';
+  import {
+    todos, deleteTodo, getTodo, doneTodoToggle, todoHasDone, editTodo, isEditing, todoItem
+    } from './composable/todo'
 
 
 
-const showModal = ref(false);
-function addTodoBtn(){
-  todoItem.value = ''
-  showModal.value = true
-}
-console.log(todos.value)
-function close(){
-  isEditing.value = false
-  todoItem.value = {}
-}
-onMounted(() => {
-  getTodo()
-})
-
-
-
+  const showModal = ref(false);
+  function addTodoBtn(){
+    todoItem.value = ''
+    showModal.value = true
+  }
+  console.log(todos.value)
+  function close(){
+    isEditing.value = false
+    todoItem.value = {}
+  }
+  onMounted(() => {
+    getTodo()
+  })
 </script>
 
 <template>
@@ -37,16 +34,21 @@ onMounted(() => {
     <TitleApp title="Tomodo" />
     <TheInformation :done="todoHasDone().length" :todos="todos.length"/>
     <main class="mb-28">
-    <ItemTodo v-for="(todo, index) in todos" :key="todo.id"
-      :priority="todo.priority"
-      :todo="todo.todo"
-      :time="todo.time"
-      :done="todo.done"
-      @done-todo="doneTodoToggle(todo.uid)"
-      @delete-todo="deleteTodo(todo.uid)"
-      @edit-todo="editTodo(todo.uid,showModal = true)"
-    >
-    </ItemTodo>
+    <div v-if="!todos.length" class="flex w-full h-[60vh] px-10 items-center">
+      <h1>Oppss No todo right now. let's create <span role="button" @click="addTodoBtn" class="underline">todo</span>.</h1>
+    </div>
+    <div v-else>
+      <ItemTodo v-for="(todo, index) in todos" :key="todo.id"
+        :priority="todo.priority"
+        :todo="todo.todo"
+        :time="todo.time"
+        :done="todo.done"
+        @done-todo="doneTodoToggle(todo.uid)"
+        @delete-todo="deleteTodo(todo.uid)"
+        @edit-todo="editTodo(todo.uid,showModal = true)"
+      >
+      </ItemTodo>
+    </div>
     </main>
     <vue-final-modal  v-model="showModal"
       :overlay-style="{
@@ -58,7 +60,7 @@ onMounted(() => {
       <MyForm />
     </vue-final-modal>
     <div class="w-full flex justify-center fixed bottom-6 ">
-      <button highlight  @click="addTodoBtn" class=" p-5 bg-[#032836] text-center text-white rounded-lg">Add Task</button>
+      <button highlight  @click="addTodoBtn" class=" p-5 bg-[#032836] text-center text-white rounded-lg">Add Now</button>
     </div>
 
   </div>
