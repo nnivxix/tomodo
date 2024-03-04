07bd4950208d91c3909129e571e1d80f41f09861
nnivxix <adhe.hanasa@gmail.com>
2024-03-04 22:26:52 +0700

    test: useTodo

--- vitest.config.js
+++ vitest.config.js
@@ -1,14 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import path from "path";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: [
-      './vitest.setup.js'
-    ],
-  }
-})
+    environment: "jsdom",
+    setupFiles: ["./vitest.setup.js"],
+  },
+});
