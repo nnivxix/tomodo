043afbdc83bf8b29516ae0540b51f7b9588b7f52
nnivxix <adhe.hanasa@gmail.com>
2024-10-03 12:12:32 +0700

    wip: highlight code example

--- src/components/ExampleJson.vue
+++ src/components/ExampleJson.vue
@@ -1,23 +1,40 @@
 <script setup>
+import { onMounted, onUpdated } from "vue";
 /** @type {import('@/types').ExampleJsonProp} */
 const { json } = defineProps({
   json: Object,
 });
+
+onMounted(() => {
+  hljs.highlightAll();
+});
+
+onUpdated(() => {
+  hljs.highlightAll();
+});
 </script>
 <template>
-  <pre
-    class="border border-gray-500 border-dashed p-3 rounded-md whitespace-pre-wrap"
-    v-if="json"
-    >{{ json }}
-      </pre
-  >
-  <pre class="border border-gray-500 border-dashed p-3 rounded-md" v-else>
-{
-  "id": "",
-  "name": "",
-  "description": "",
-  "todos": []
-}
-      </pre
-  >
+  <template v-if="json">
+    <pre
+      class="border border-gray-500 border-dashed p-3 rounded-md whitespace-pre-wrap"
+    >
+      <code class="language-json">
+        {{ json }}
+        </code>
+        </pre>
+  </template>
+  <template v-else>
+    <pre
+      class="border border-gray-500 border-dashed p-3 rounded-md whitespace-pre-wrap"
+    >
+      <code class="language-json">
+        {
+          "id": "",
+          "name": "",
+          "description": "",
+          "todos": []
+        }
+      </code>
+        </pre>
+  </template>
 </template>
